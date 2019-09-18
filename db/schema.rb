46890df7c8e4f2361f2c1188e2bb87ccb5ac0347
Rebeca Arroyo <rarroyo2014@fau.edu>
2019-09-18 21:00:30 +0000

    GEOCODE

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_09_16_183528) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.float "latitute"
+    t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
