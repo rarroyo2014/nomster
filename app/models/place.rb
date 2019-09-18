1245b76509f0054de755653af3f9dbd0de2f5a8b
Rebeca Arroyo <rarroyo2014@fau.edu>
2019-09-18 20:16:13 +0000

    Lat and lng

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true 
+  geocoded_by :address
+  after_validation :geocode
+
+  validates :name, presence: true  
 end
