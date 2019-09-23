2adfd7466abfde179a53eb36c85cd982a98021bc
Rebeca Arroyo <rarroyo2014@fau.edu>
2019-09-23 21:26:06 +0000

    first

--- db/migrate/20190923191927_alter_places_add_photo_id_column.rb
+++
@@ -1,5 +0,0 @@
-class AlterPlacesAddPhotoIdColumn < ActiveRecord::Migration[5.2]
-  def change
-    add_column :places, :photo_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_191927) do
+ActiveRecord::Schema.define(version: 2019_09_19_171801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,11 +39,10 @@ ActiveRecord::Schema.define(version: 2019_09_23_191927) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.float "latitude"
     t.float "longitude"
-    t.integer "photo_id"
-    t.integer "photo"
-    t.index ["photo_id"], name: "index_places_on_photo_id"
+    t.index ["user_id"], name: "index_places_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
